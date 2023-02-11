4335beddc3d608678e807c091facd564f8bbd218
michael <loganmitchellahaha@gmail.com>
2023-02-11 18:11:05 +0200

    Second Commit

--- movieDB.js
+++ movieDB.js
@@ -9,12 +9,23 @@ const personalMovieDB = {
     private: false
 };
 
-const a = prompt('Very latest watched movie?', ''),
-      b = prompt('What is your grade', ''),
-      c = prompt('Very latest watched movie?', ''),
-      d = prompt('What is your grade', '');
+for (let i = 0; i < 2; i++){
+    const a = prompt('Very latest watched movie?', ''),
+          b = prompt('What is your grade', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != "" && b != "" && a.length < 10){
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10){
+    console.log("few movies has been watched");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ){
+    console.log("typical viewer");
+} else{
+    console.log('movielower');
+}
 
 console.log(personalMovieDB);
